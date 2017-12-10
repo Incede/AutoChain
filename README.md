0fce9154ccfd5871e1ff0523fdedc6162b832504
Incede <incede@protonmail.com>
2017-12-10 17:57:23 +0530

    Init Auto Chain

---
+++ README.md
@@ -0,0 +1,37 @@
+#
+
+# Auto Chain
+
+Registry for cars new and old
+
+Marketplace for used cars
+
+auto sales and lead acquisition, 
+vehicle history reports, 
+auto marketing and auto financing
+
+
+
+# Auto Attributes                 Participant attributes
+
+make					regulator
+model					manufactrer
+colour					dealership
+reg					lease company
+owner					leasee	
+scrapped				scrap merchant
+status
+leasecontractid
+
+
+# Stages of Auto
+
+
+    Vehicle is created as a template by the regulator.
+    Vehicle template is transferred to the manufacturer.
+    Manufacturer updates the vehicle template to define it as a vehicle giving it a make, model, reg etc.
+    Manufacturer transfers the vehicle to dealership to be sold.
+    Dealership transfers the vehicle to a lease company.
+    Lease company transfers the vehicle to a leasee. The vehicle is not leased instead the application is showing what would happen if the lease were to have come to an end and the leasee activated the purchase option.
+    Leasee transfers the vehicle to a scrap merchant so that it can be scrapped.
+    Scrap merchant scraps the vehicle.
---
+++ contracts/Auto1.sol
@@ -0,0 +1,58 @@
+/*  starting functions for Auto creation etc.*/
+
+pragma solidity ^0.4.19;
+
+contract Auto {
+
+//array that maintains registration Id's of all autos
+bytes32[] Reg;    
+
+//Struct contains attributes of each auto
+struct attributes{
+bytes32 make;
+bytes32 model;
+bytes32 colour;
+bytes32 owner;
+bool scrapped;
+bytes32 leaseid;
+}
+
+struct participants {
+bytes32	regulator;
+bytes32	manufactrer;
+bytes32	dealership;
+bytes32	lease_company;
+bytes32	leasee;
+bytes32	scrap_merchant;
+}
+
+// List that maps each auto reg id with its features & participants
+mapping (bytes32=> attributes) Autolist;
+mapping (bytes32=>participants) Partylist;
+
+function register_new_auto (bytes32 regid)  public returns (bool) {
+index = Reg.length ;
+Reg [index] = regid;
+return true; 
+}
+
+function get_auto (bytes32 regid) public constant returns (attributes) {
+return Autolist[regid];	
+}
+
+function set_auto (bytes32 regid, bytes32 make, bytes32 model, bytes32 colour, bytes32 owner, bytes32 leaseid) public {
+Autolist[regid].make = make;
+Autolist[regid].model = model;
+Autolist[regid].colour = colour;
+Autolist[regid].owner = owner;
+Autolist[regid].leaseid = leaseid;
+}
+
+function transfer_auto (bytes32 regid, bytes32 new_owner) public {
+Autolist[regid].owner = new_owner;
+}
+
+//function change_auto_status (string regid, bool status) {}
+
+
+}
